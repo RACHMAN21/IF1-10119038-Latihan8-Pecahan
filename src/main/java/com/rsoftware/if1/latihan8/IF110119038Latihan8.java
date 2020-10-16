20ae7f6701e9ce70b5dca15b25437ae36c1f6eed
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 15:50:27 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan8/IF110119038Latihan8.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan8;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF1
+ * NIM   : 10119038
+ * Deskripsi Program : Menampilkan angka dalam bentuk bilangan pecahan
+ * 
+ */
+
+public class IF110119038Latihan8 {
+    
+    public static void main(String[] args) {
+        
+        float f;
+        double d;
+        
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+}
